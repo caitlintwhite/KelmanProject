42c300f8989eb4ed8f1d2d8cb98e78abb1deec11
Emily Kelman <emilykelman@cu-biot-1-10.203.128.33.int.colorado.edu>
2019-01-15 14:53:15 -0700

    read in datasets, percent species in bos

--- exploratory_analysis/BOS_trait_species_overlap.R
+++ exploratory_analysis/BOS_trait_species_overlap.R
@@ -7,3 +7,13 @@ library(readxl)
 #read in dataset
 bos_dat <- read_csv("/Users/emilykelman/Google\ Drive/KelmanProject/tgsna_monitoring_19912016.csv")
 trait_dat <- read_xlsx("/Users/emilykelman/Google\ Drive/KelmanProject/Trait_species_list_veg_dormancy.xlsx")
+
+#look at structure of data frames
+glimpse(bos_dat) #tidyverse function
+str(bos_dat) #base R function
+glimpse(trait_dat)
+
+#what species in trait dataset are in bos dataset (without data cleaning)
+trait_species <- unique(trait_dat$Species..1)
+bos_species <- unique(bos_dat$OSMP_Code)
+summary(trait_species %in% bos_species)
