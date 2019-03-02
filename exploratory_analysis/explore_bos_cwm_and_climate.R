59e786ddab80098944b77bb0b111e06b6a7da7e5
caitlintwhite <caitlin.t.white@colorado.edu>
2019-03-02 13:41:56 -0700

    update CWM tables and figs for transect correction in 2004

--- exploratory_analysis/explore_bos_cwm_and_climate.R
+++ exploratory_analysis/explore_bos_cwm_and_climate.R
@@ -80,7 +80,7 @@ table(trait_species %in% spp_lookup$JL_Code) #of trait spp, how many overlap wit
 summary(bos_species %in% spp_lookup$OSMP_Code.clean) #of bos pp, how many overlap with trait species?
 
 # which trait species are not in bos dataset?
-bos_species[which(!bos_species %in% spp_lookup$OSMP_Code.clean)]
+sort(bos_species[which(!bos_species %in% spp_lookup$OSMP_Code.clean)])
 
 
 
@@ -103,14 +103,11 @@ yrs <- c(1991:2016) # yrs in dataset range 1991-2016
 
 # specify whether want to do individual transect CWM (pool_CWM = FALSE) or "regional" pooled community CWM (pool_CWM = TRUE)
 # if pooled, will sum spp abundances across all transects, by year, before creating abundance matrix
-pool_CWM <- TRUE
+pool_CWM <- FALSE
 
 # specify vector of desired traits
 traits <- colnames(trait_dat)[c(7,10,12:14,17,18,35)] # this will be only line to modify, everything below here generic
 traits # visually check if traits you expected
-
-# specify climate variables desired
-climvars <- c("precip_5", "spei_5", "avgtmax_5", "avgtmin_5", "tmean_5", "spei_12", "tmean_12", "precip_12", "gs_halfprecip_doy", "june1_totalppt", "gs_dry_days", "gs_days_rain")
 
 # create vector of spp codes that exist in both veg data and trait data for selected sites, yrs, and traits
 abundance_spp <- with(bos_dat, unique(OSMP_Code.clean[transect_ID.clean %in% transects & Year %in% yrs]))
@@ -196,10 +193,16 @@ bos_cwm$Year <- as.numeric(gsub("[0-9].*[0-9][.]", "", bos_cwm$sitekey)) #Year w
 # extract transect_ID.clean from sitekey only if pool_CWM is FALSE (i.e. it's an transect-level CWM)
 if(pool_CWM==FALSE){
   bos_cwm$transect_ID.clean <- gsub("[.].*$", "", bos_cwm$sitekey) #everything after period in sitekey is removed
+  # re-order columns (sitekey, Year and transect ID before trait CWMs)
+  bos_cwm <- dplyr::select(bos_cwm, sitekey,Year, transect_ID.clean, traits)
+  #assign bos_cwm to object with "transect" in it so know what you're plotting and writing out below
+  transect_CWM <- bos_cwm
+} else{
+  # re-order columns (no transect_ID to select)
+  bos_cwm <- dplyr::select(bos_cwm, sitekey:Year, traits)
+  #assign bos_cwm to an object name with "pool" in it so know what you're plotting and writing out below
+  pooled_CWM <- bos_cwm
 }
-# re-order columns (transect_ID.clean alphabetically comes between sitekey and Year so this line will always work)
-bos_cwm <- dplyr::select(bos_cwm, sitekey:Year, traits)
-
 
 # -- WRITE OUT DATA FRAMES FOR MODELING USE -----
 # write out abundance and bos_cwm data frames for use in modeling
@@ -208,16 +211,13 @@ if(pool_CWM){
   # pooled abundance matrix
   write.csv(rel_abundance, paste0(gdrive,"/KelmanProject/Data/bos_pooled_relabundance.csv"), row.names = T)
   # pooled CWMs, annually
-  write.csv(bos_cwm, paste0(gdrive,"/KelmanProject/Data/bos_pooled_CWM.csv"), row.names = F)
-  #assign bos_cwm to an object name with "pool" in it so know what you're plotting and writing out below
-  pooled_CWM <- bos_cwm
+  write.csv(pooled_CWM, paste0(gdrive,"/KelmanProject/Data/bos_pooled_CWM.csv"), row.names = F)
 }else{
   # write tranect-yr abundance matrix
   write.csv(rel_abundance, paste0(gdrive,"/KelmanProject/Data/bos_transect_relabundance.csv"), row.names = T)
   # transect level CWM, annually
-  write.csv(bos_cwm, paste0(gdrive,"/KelmanProject/Data/bos_transect_CWM.csv"), row.names = F)
-  #assign bos_cwm to object with "transect" in it so know what you're plotting and writing out below
-  transect_CWM <- bos_cwm
+  write.csv(transect_CWM, paste0(gdrive,"/KelmanProject/Data/bos_transect_CWM.csv"), row.names = F)
+  
 }
 # write out functional trait data frame (stays the same whether pooled or transect-level)
 write.csv(fxnl_df, paste0(gdrive,"/KelmanProject/Data/fxnl_trait_community_df.csv"), row.names = T)
@@ -227,23 +227,41 @@ write.csv(fxnl_df, paste0(gdrive,"/KelmanProject/Data/fxnl_trait_community_df.cs
 # -- EXPLORE CWM WITH OTHER DATASETS -----
 ##########################################
 
+# specify climate variables desired
+climvars <- c("precip_5", "spei_5", "avgtmax5", "avgtmin5", "tmean_5", "spei_12", "tmean_12", "precip_12", "gs_halfprecip_doy", "june1_totalppt", "gs_days_dry", "gs_days_rain")
+
 # can visualize relationship between bos CWM and environment.. CWM to total annual cover.. whatever you choose..
-#wide form for creating linear regression
-CWM_climate_merged_W <- left_join(transect_cwm, clim_dat[c("year", climvars)],  by=c("Year"="year")) 
-#write csv
-write.csv(CWM_climate_merged_W, paste0(gdrive,"/KelmanProject/Data/transectCWM_for_regressions.csv"), row.names = T)
+# name data frames differently depending on whether pooled or transect-level
+if(pool_CWM){
+  #pooled CWM + climate wide form for creating linear regression
+  poolCWM_climate_merged_W <- left_join(pooled_CWM, clim_dat[c("year", climvars)],  by=c("Year"="year")) 
+  #write csv
+  write.csv(poolCWM_climate_merged_W, paste0(gdrive,"/KelmanProject/Data/pooledCWM_for_regressions.csv"), row.names = F)
+  
+  #pooled CWM + climate long form for creating figures 
+  poolCWM_climate_merged_L <- left_join(pooled_CWM, clim_dat[c("year", climvars)],  by=c("Year"="year"))%>%
+    gather( key = "trait_name", value, traits, climvars)
+  #write csv
+  write.csv(poolCWM_climate_merged_L, paste0(gdrive,"/KelmanProject/Data/pooledCWM_for_figures.csv"), row.names = F)  
 
-#long form for creating figures 
-CWM_climate_merged_L <- left_join(transect_cwm, clim_dat[c("year", climvars)],  by=c("Year"="year"))%>%
-  gather( key = "trait_name", value, traits, climvars)
+}else{
+  #transect CWM + climate wide form for creating linear regression
+  tranCWM_climate_merged_W <- left_join(transect_CWM, clim_dat[c("year", climvars)],  by=c("Year"="year")) 
+  #write csv
+  write.csv(tranCWM_climate_merged_W, paste0(gdrive,"/KelmanProject/Data/transectCWM_for_regressions.csv"), row.names = F)
+  
+  #transect CWM + climate long form for creating figures 
+  tranCWM_climate_merged_L <- left_join(transect_CWM, clim_dat[c("year", climvars)],  by=c("Year"="year"))%>%
+    gather( key = "trait_name", value, traits, climvars)
+  #write csv
+  write.csv(tranCWM_climate_merged_L, paste0(gdrive,"/KelmanProject/Data/transectCWM_for_figures.csv"), row.names = F)
+}
 
-#write csv
-write.csv(CWM_climate_merged_L, paste0(gdrive,"/KelmanProject/Data/transectCWM_for_figures.csv"), row.names = T)
 
 #=====TRANSECT LEVEL CWM FIGURES AND LM=====
 
 #create figure to look at LDMC and precip 
-area7_LDMC_precip_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=precip_5, y=LDMC))+
+area7_LDMC_precip_fig <- ggplot(tranCWM_climate_merged_W, mapping = aes(x=precip_5, y=LDMC))+
   geom_point(aes(col=transect_ID.clean))+
   geom_smooth(method = "lm", col ="black") +
   geom_smooth(aes(col=transect_ID.clean), method = "lm", se = F)
@@ -251,11 +269,11 @@ area7_LDMC_precip_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=precip_5, 
 area7_LDMC_precip_fig
 
 #running linear regression on LDMC and precip_5
-LDMC_precip_LM <- lm(formula = LDMC ~ precip_5, data = CWM_climate_merged_W)
+LDMC_precip_LM <- lm(formula = LDMC ~ precip_5, data = tranCWM_climate_merged_W)
 summary(LDMC_precip_LM)
 
 #create figure to look at mean temp over growing season and SLA
-SLA_tmean5_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=tmean_5, y=SLA))+
+SLA_tmean5_fig <- ggplot(tranCWM_climate_merged_W, mapping = aes(x=tmean_5, y=SLA))+
   geom_point(aes(col=transect_ID.clean))+
   geom_smooth(method = "lm", col="black")+
   geom_smooth(aes(col=transect_ID.clean), method = "lm", se = F)
@@ -263,11 +281,11 @@ SLA_tmean5_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=tmean_5, y=SLA))+
 SLA_tmean5_fig
 
 #run linear regression for SLA and tmean_5. pvalue .18 and r^2 value .003
-SLA_tmean5_LM <- lm(formula = SLA ~ tmean_5, data = CWM_climate_merged_W)
+SLA_tmean5_LM <- lm(formula = SLA ~ tmean_5, data = tranCWM_climate_merged_W)
 summary(SLA_tmean5_LM)
 
 #plotting tmin_5 and RDMC
-RDMC_tmin5_fig <-ggplot(CWM_climate_merged_W, mapping = aes(x=tmin_5, y=RDMC))+
+RDMC_tmin5_fig <-ggplot(tranCWM_climate_merged_W, mapping = aes(x=avgtmin5, y=RDMC))+
   geom_point(aes(col=transect_ID.clean))+
   geom_smooth(method = "lm", col="black")+
   geom_smooth(aes(col=transect_ID.clean), method = "lm", se = F)
@@ -275,11 +293,11 @@ RDMC_tmin5_fig <-ggplot(CWM_climate_merged_W, mapping = aes(x=tmin_5, y=RDMC))+
 RDMC_tmin5_fig
 
 #linear regression for tmin_5 and RDMC. pvalue of .72 and r^2 value -.004 
-RDMC_tmin5_LM <- lm(formula = RDMC ~ tmin_5, data = CWM_climate_merged_W)
-summary(RDMC_tmin5_LM)
+RDMC_tmin5_LM <- lm(formula = RDMC ~ avgtmin5, data = tranCWM_climate_merged_W) 
+summary(RDMC_tmin5_LM) # ctw, 3/2/19: using growing season average tmin has slightly better relationship with RDMC
 
 #plot root mass ratio and spei_12. p value .5 and r^2 value of -.002
-RMR_spei12_fig <-ggplot(CWM_climate_merged_W, mapping = aes(x=spei_12, y=RMR))+
+RMR_spei12_fig <-ggplot(tranCWM_climate_merged_W, mapping = aes(x=spei_12, y=RMR))+
   geom_point(aes(col=transect_ID.clean))+
   geom_smooth(method = "lm", col="black")+
   geom_smooth(aes(col=transect_ID.clean), method = "lm", se = F)
@@ -287,18 +305,18 @@ RMR_spei12_fig <-ggplot(CWM_climate_merged_W, mapping = aes(x=spei_12, y=RMR))+
 RMR_spei12_fig
 
 #linear regression for RMR and spei_12
-RMR_spei12_LM<- lm(formula = RMR ~ spei_12, data = CWM_climate_merged_W)
+RMR_spei12_LM<- lm(formula = RMR ~ spei_12, data = tranCWM_climate_merged_W)
 summary(RMR_spei12_LM)
 
 #plotting precipitation over time. commenting it out because it's included in the panel figure
 #precip_overT_fig <-ggplot(clim_dat, mapping = aes(x=year, y=precip_5)) +
-geom_line()+
-  geom_point()
+#geom_line()+
+#  geom_point()
 
 #precip_overT_fig
 
 #plot final plant height and spei_12
-finalheight_spei12_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=spei_12, y=final_height_cm))+
+finalheight_spei12_fig <- ggplot(tranCWM_climate_merged_W, mapping = aes(x=spei_12, y=final_height_cm))+
   geom_point(aes(col=transect_ID.clean))+
   geom_smooth(method = "lm", col="black")+
   geom_smooth(aes(col=transect_ID.clean), method = "lm", se = F)
@@ -306,40 +324,47 @@ finalheight_spei12_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=spei_12, 
 finalheight_spei12_fig
 
 #linear regression for final plant height and spei_12. pvalue .03 and r^2 value .01
-finalheight_spei12_LM <- lm(formula = final_height_cm ~ spei_12, data = CWM_climate_merged_W)
+finalheight_spei12_LM <- lm(formula = final_height_cm ~ spei_12, data = tranCWM_climate_merged_W)
 summary(finalheight_spei12_LM)
 
 #plotting drought index over time. Also commenting out because it's included in the panel plot 
 #drought_overT_fig <- ggplot(clim_dat, mapping = aes(x=year, y=spei_12))+
-geom_col()
+#geom_col()
 
 #drought_overT_fig
 
 
 #plot CWM for SLA and precip over time
-ggplot(subset( CWM_climate_merged_L, trait_name%in% c("SLA", "precip_5", "spei_5", "RMR")), mapping = aes(Year, value))+
-  geom_line()+
+SLA_RMR_env_fig <- ggplot(subset(tranCWM_climate_merged_L, trait_name%in% c("SLA", "gs_halfprecip_doy", "spei_5", "RMR")), mapping = aes(Year, value))+
+  # add line only to panels that have single value per year
+  geom_line(data = subset(tranCWM_climate_merged_L, trait_name%in% c("gs_halfprecip_doy", "spei_5")), aes(Year, value)) +
+  # plot points for all panels
   geom_point()+
+  # add mean trend line for plan trait panels
+  stat_summary(data = subset(tranCWM_climate_merged_L, trait_name%in% c("RMR", "SLA")),
+               aes(y = value), fun.y=mean, colour="red", lwd=1, geom="line") +
+  # adjust year scaling (add major gridlines at more years)
+  scale_x_continuous(breaks = seq(1990, 2016, 4)) +
   facet_grid(trait_name~., scales = "free_y")+
   theme_bw()
+SLA_RMR_env_fig 
 
-#======AREA 7 CWM FIGURES AND LM=====
+
+#======AREA 7 POOLED CWM FIGURES AND LM=====
 #graph pooled cwm final height and spei_12
-pooledCWM_height_spei12_fig <-ggplot(CWM_climate_merged_W, mapping = aes(x=spei_12, y=final_height_cm))+
+pooledCWM_height_spei12_fig <-ggplot(poolCWM_climate_merged_W, mapping = aes(x=spei_12, y=final_height_cm))+
   geom_point()+
-  geom_smooth(method = "lm", col="black")+
   geom_smooth(method = "lm", se = F)
 
 pooledCWM_height_spei12_fig
 
 #linear regression. p value of .06 and r^2 .10
-pooledLM_height_spei12<- lm(formula = final_height_cm ~ spei_12, data = CWM_climate_merged_W)
+pooledLM_height_spei12<- lm(formula = final_height_cm ~ spei_12, data = poolCWM_climate_merged_W)
 summary(pooledLM_height_spei12)
 
 #graph pooled cwm LDMC and precip_12
-pooledCWM_precip12_LDMC_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=precip_12, y=LDMC))+
+pooledCWM_precip12_LDMC_fig <- ggplot(poolCWM_climate_merged_W, mapping = aes(x=precip_12, y=LDMC))+
   geom_point()+
-  geom_smooth(method = "lm", col ="black") +
   geom_smooth(method = "lm", se = F)
 
 pooledCWM_precip12_LDMC_fig
@@ -347,50 +372,48 @@ pooledCWM_precip12_LDMC_fig
 #linear regression pooledCWM LDMC and precip_12
 #p value .18 and r^2 .03
 #idea to make panel plot of RDMC and LDMC? 
-pooledLM_precip12_LDMC <- lm(formula = LDMC ~ precip_12, data = CWM_climate_merged_W)
+pooledLM_precip12_LDMC <- lm(formula = LDMC ~ precip_12, data = poolCWM_climate_merged_W)
 summary(pooledLM_precip12_LDMC)
 
 
-pool_tmax5_SLA_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=tmax_5, y=SLA))+
+pool_tmax5_SLA_fig <- ggplot(poolCWM_climate_merged_W, mapping = aes(x=avgtmax5, y=SLA))+
   geom_point()+
-  geom_smooth(method = "lm", col ="black") +
-  geom_smooth(method = "lm", se = F)
+  geom_smooth(method = "lm")
 
 pool_tmax5_SLA_fig
 
-#pooled linear model tmax and SLA
-#p value .2 and r^2 .02
-pooledLM_tmax5_SLA <- lm(formula = SLA ~ tmax_5, data = CWM_climate_merged_W)
+#pooled linear model avg tmax and SLA
+#p value .2 and r^2 .02 (for absolute tmax)
+#p value .3 and r^2 0.005 for avg growing season tmax
+pooledLM_tmax5_SLA <- lm(formula = SLA ~ avgtmax5, data = poolCWM_climate_merged_W)
 summary(pooledLM_tmax5_SLA)
 
-poolRMR_spei12_fig <- ggplot(CWM_climate_merged_W, mapping = aes(x=spei_12, y=RMR))+
+poolRMR_spei12_fig <- ggplot(poolCWM_climate_merged_W, mapping = aes(x=spei_12, y=RMR))+
   geom_point()+
-  geom_smooth(method = "lm", col ="black")+
-  geom_smooth(method = "lm", se = F)
+  geom_smooth(method = "lm")
 
 poolRMR_spei12_fig
 
 #linear model pooled RMR and spei12
 #p value .06 and adjusted r^2 .10
-pooledLM_spei12_RMR <-lm(formula = RMR ~ spei_12, data = CWM_climate_merged_W)
+pooledLM_spei12_RMR <-lm(formula = RMR ~ spei_12, data = poolCWM_climate_merged_W)
 summary(pooledLM_height_spei12)
 
 #panel plot of spei and traits over time 
-spei5_traits_overT_panel <-ggplot(subset( CWM_climate_merged_L, trait_name%in% c("spei_5", "RMR", "SLA", "RDMC", "seed_mass", "LDMC")), mapping = aes(Year, value))+
+spei5_traits_overT_panel <-ggplot(subset(poolCWM_climate_merged_L, trait_name%in% c("spei_5", "RMR", "SLA", "RDMC", "seed_mass", "LDMC")), mapping = aes(Year, value))+
   geom_line()+
   geom_point(size = 0.5)+
-  geom_point(data = subset(CWM_climate_merged_L, trait_name %in% "spei_5"), aes(Year, value, col = value >= 0)) +
+  geom_point(data = subset(poolCWM_climate_merged_L, trait_name %in% "spei_5"), aes(Year, value, col = value >= 0)) +
   scale_x_continuous(breaks = seq(1992, 2016, by = 2))+
   scale_color_discrete(guide = "none") +
   theme(axis.text.x = element_text(angle=45, hjust = 1))+
   facet_grid(trait_name~., scales = "free_y")
 
-
-
 spei5_traits_overT_panel
 
+
 #plot changes in environmental variables over time (tmean_12, precip_12, spei_12)
-Enviro_variables_overT_panel <-ggplot(subset( CWM_climate_merged_L, trait_name%in% c("precip_12", "tmean_12", "spei_12")), mapping = aes(Year, value))+
+Enviro_variables_overT_panel <-ggplot(subset(poolCWM_climate_merged_L, trait_name%in% c("precip_12", "tmean_12", "spei_12")), mapping = aes(Year, value))+
   geom_line()+
   geom_point(size=.5)+
   scale_x_continuous(breaks = seq(1992, 2016, by = 2))+
@@ -408,6 +431,7 @@ ggsave("./exploratory_analysis/figures/area7_LDMC_fig.pdf", area7_LDMC_precip_fi
 ggsave("./exploratory_analysis/figures/SLA_tmean5_fig.pdf", SLA_tmean5_fig)
 ggsave("./exploratory_analysis/figures/RDMC_tmin5_fig.pdf", RDMC_tmin5_fig)
 ggsave("./exploratory_analysis/figures/RMR_spei12_fig.pdf", RMR_spei12_fig )
+ggsave("./exploratory_analysis/figures/SLA_RMR_env_panel.pdf", SLA_RMR_env_fig)
 ggsave("./exploratory_analysis/figures/finalheight_spei12_fig.pdf", finalheight_spei12_fig)
 ggsave("./exploratory_analysis/figures/Enviro_variables_overT_panel.pdf", Enviro_variables_overT_panel)
 ggsave("./exploratory_analysis/figures/spei5_traits_overT_panel.pdf", spei5_traits_overT_panel)
